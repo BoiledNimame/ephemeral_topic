106b3a5d7c8acf5b7a8fa88712a35135cc0b4ac6
--global <haduki.kyouka@gmail.com>
2023-07-12 02:40:32 +0900

    add / todo, logger

--- ephtopic/src/main/java/com/ephemeraltopic/Main.java
+++ ephtopic/src/main/java/com/ephemeraltopic/Main.java
@@ -1,6 +1,34 @@
 package com.ephemeraltopic;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main
 {
-    
+    public static Logger logger;
+
+    private Main( String[] args )
+    {
+        logger = LogManager.getLogger();
+        //TODO split and get token by args[]
+    }
+
+    public static void main ( String[] args )
+    {
+        if ( args==null || args.length==0 )
+        {
+            exit(1);
+        }
+        else
+        {
+            Main BotInstance = new Main( args );
+        }
+    }
+
+    public static void exit ( int code )
+    {
+        // TODO shutdown jda object
+        System.exit(code);
+    }
 }
